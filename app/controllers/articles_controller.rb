14ddafbb9682644a2d7667111eec88208e590602
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-03-10 16:16:15 +0800

    Refactor code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,13 +13,12 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article was created successfuly."
+      set_notice_msg("created")
       redirect_to @article
     else
       render 'new'
@@ -27,9 +26,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
-      flash[:notice] = "Article was updated successfuly."
+    if @article.update(article_params)
+      set_notice_msg("updated")
       redirect_to @article
     else
       render 'edit'
@@ -37,11 +35,24 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     if @article.destroy
-      flash[:notice] = "Article was created deleted."
+      set_notice_msg("deleted")
     end
     redirect_to articles_path
   end
 
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def set_notice_msg(msg)
+    flash[:notice] = "Article was #{ msg } successfuly."
+  end
+  
 end
