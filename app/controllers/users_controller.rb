7ab217f26aa9be2d7ff49221da7d37a65c7df295
Nina Michelle Magpuyo <michellemagpuyo@gmail.com>
2021-03-12 11:38:50 +0800

    Add helper for profile's image and add associated articles in show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def new
     @user = User.new
   end
 
-  def create
+  def show
+    @articles = @user.articles
+  end
+
+  def edit
+  end
+
+  def create 
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "Welcome to Alpha Blog #{ @user.username }, you have successfuly signed up."
@@ -14,10 +22,26 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @article.update(user_params)
+      flash[:notice] = "Your account information was successfully updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
 
+  def set_notice_msg(msg)
+    flash[:notice] = "User was #{ msg } successfuly."
+  end
 end
